f973ff9725664ee49ded603da2306ea9c855c977
Alex Nuzhniy <alex.nuzhniy@gmail.com>
2017-03-16 12:19:45 +0200

    Initial commit

---
+++ 8ball.rb
@@ -0,0 +1,46 @@
+
+greetings = [
+	"Hello, my dear friend! I'm answering you question...",
+	"The one asking will always receive the answer",
+	"Hey, mortal. Today we have the following answer for you"
+]
+
+answers = [
+	# Положительные
+	"It is certain",
+	"It is decidedly so",
+	"Without a doubt", 
+	"Yes — definitely", 
+	"You may rely on it", 
+	
+	# Нерешительно положительные
+	"As I see it, yes", 
+	"Most likely", 
+	"Outlook good", 
+	"Signs point to yes", 
+	"Yes",
+
+	# Нейтральные
+	"Reply hazy, try again", 
+	"Ask again later", 
+	"Better not tell you now", 
+	"Cannot predict now", 
+	"Concentrate and ask again", 
+	
+	# Отрицательные
+	"Don’t count on it", 
+	"My reply is no", 
+	"My sources say no", 
+	"Outlook not so good", 
+	"Very doubtful"
+]
+
+# Choose random greeting
+puts greetings.sample
+puts
+
+# Adding a delay before answer
+sleep 2
+
+# Print random answer
+puts answers.sample
